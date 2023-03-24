f3e914d5a66ec302b430447aea00bf949411c67c
Nichole-Murphy <nicholejmurphy@gmail.com>
2023-03-24 11:32:08 -0400

    created view functions for user and related empty html files.

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ app.app_context().push()
 
 connect_db(app)
 
+
+##########################################################################
 # User signup/login/logout
-
-
 @app.before_request
 def add_user_to_global():
     """If user is logged in, add to Flask Global"""
@@ -42,12 +42,57 @@ def do_login(user):
     session[CURR_USER_ID] = user.id
 
 
-def do_logout(user):
+def do_logout():
 
     if CURR_USER_ID in session:
         del session[CURR_USER_ID]
 
 
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    """Handle signup."""
+
+    form = UserSignUpForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.signup(username=form.username.data, first_name=form.first_name.data,
+                               last_name=form.last_name.data, password=form.password.data)
+            db.session.commit()
+        except IntegrityError:
+            flash("Username is already taken.", "danger")
+            return render_template('users/signup.html', form=form)
+
+        do_login(user)
+        return redirect('/')
+
+    return render_template('user/signup.html', form=form)
+
+
 @app.route('/login', methods=["GET", "POST"])
 def login():
     """Handle user  login."""
+
+    form = UserLoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.username.data, form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Welcome {user.first_name}!", "success")
+            return redirect('/')
+
+        flash("Invalid username or password.", "danger")
+
+    return render_template("templates/users/login.html", form=form)
+
+
+@app.route('/logout')
+def logout():
+    """Handles user logout."""
+
+    do_logout()
+    flash("Successfully logget out. Happy trails!", "success")
+
+    return redirect("/")
